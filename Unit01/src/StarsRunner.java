9fb09070c64486b90cf92c51c83948c5047c7d12
julialluo <47190039+julialluo@users.noreply.github.com>
2019-02-01 11:02:11 -0800

    finished unit01

---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,29 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      
+      //call the methods needed to make the patterns on the word document
+	   
+	   StarsAndStripes ref = new StarsAndStripes();
+
+	 //example method call
+	   ref.printASmallBox();
+	   ref.printABigBox();
+
+	 //add more methods calls
+
+      
+   }
+}
