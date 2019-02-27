5aac860f866ee71811c7a85acdb7db1cd8ddfb1a
julialluo <47190039+julialluo@users.noreply.github.com>
2019-02-27 11:56:20 -0800

    updated unit8

---
+++ Unit08/src/RaySumLast.java
@@ -0,0 +1,19 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class RaySumLast
+{
+	public static int go(int[] ray)
+	{
+		int lastValue = ray[ray.length-1];
+		
+		int sum = 0;
+		
+		for (int i = 0; i++)
+		return 0;
+	}
+	
+	
+}
